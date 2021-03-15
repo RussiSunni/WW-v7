f405b058b36817270872549c1c70ed1bf0244bdd
Jonathan Dyason <jonathandyason@hotmail.com>
2021-03-15 17:04:38 +0200

    asd

---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSrc;
+    public AudioClip hello;
+    void Start()
+    {
+
+    }
+
+    public void playSound(AudioClip audioclip)
+    {
+        audioSrc.PlayOneShot(audioclip);
+    }
+}
---
+++ Assets/Scripts/Subject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subject : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        animator.SetBool("isCat", false);
+    }
+
+    public void NoAnimation()
+    {
+        animator.SetBool("isCat", false);
+    }
+
+    public void Animation(string word)
+    {
+        animator.SetBool(word, true);
+        GameControl.currentWords.Clear();
+    }
+}
