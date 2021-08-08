3824f57813717cd134fa839f131fb3d995d758ec
Jonathan Dyason <jonathandyason@hotmail.com>
2021-08-08 17:34:46 +0200

    asd

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartMenu : MonoBehaviour
+{
+    public void LoadWithoutAI()
+    {
+        SceneManager.LoadScene("Without AI");
+    }
+
+    public void LoadWithAI()
+    {
+        SceneManager.LoadScene("With AI");
+    }
+}
+
+
+
+
